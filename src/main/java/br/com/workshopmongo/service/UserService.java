9967ee5667994f325e441d6a08820604f80fd77f
Lucas Banin <lucastb1988>
2018-09-20 15:34:07 -0300

    Inserção, Remoção e Atualização de usuarios

--- src/main/java/br/com/workshopmongo/service/UserService.java
+++ src/main/java/br/com/workshopmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.workshopmongo.domain.User;
+import br.com.workshopmongo.dto.UserDTO;
 import br.com.workshopmongo.repository.UserRepository;
 import br.com.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -23,6 +24,30 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		userRepository.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId()); //buscar objeto original no BD
+		updateData(newObj, obj); //atualizar o objeto antigo buscado no BD com o objeto novo
+		return userRepository.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }
